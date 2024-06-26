24d15bcdb378df7f86ad2a3f88888f1fd70485f0
“AlenaSazonava” <alenka03.92@mail.ru>
2024-06-26 16:38:30 +0300

    fix: fix attribute error and add smooth transition animation for card switching

--- src/components/generic/genericOffersBlock/GenericOffersBlock.style.js
+++ src/components/generic/genericOffersBlock/GenericOffersBlock.style.js
@@ -44,9 +44,8 @@ export const OffersCardsContainer = styled.div`
     align-items: center;
     padding-left: 25px;
     padding-right: 25px;
-    opacity: ${({ sliding }) => (sliding ? 0 : 1)};
+    opacity: ${({ $sliding }) => ($sliding ? 0 : 1)};
     transition: opacity 0.5s ease-in-out;
-    transition-property: opacity, transform;
 
     @media (max-width: 1100px) {
         flex-wrap: wrap;
